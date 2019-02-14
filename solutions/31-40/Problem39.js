e31dc87c03171bd702da337053dc3c4dfbab26c5
Victor Li <victorli5674@yahoo.com>
2019-02-13 22:16:43 -0800

    Finish 39

--- solutions/31-40/Problem39.js
+++ solutions/31-40/Problem39.js
@@ -19,16 +19,106 @@
 //
 // https://leetcode.com/problems/game-of-life/
 
-// function printBoard(liveCells) {}
+/**
+ * Returns a board with liveCells
+ * @param {number[][]} liveCells
+ * @return {string[][]}
+ */
+function createBoard(liveCells) {
+  let xMax = Number.MIN_SAFE_INTEGER;
+  let yMax = Number.MIN_SAFE_INTEGER;
 
-// function iterate(liveCells) {}
+  for (let i = 0; i < liveCells.length; i++) {
+    const [x, y] = liveCells[i];
+    xMax = Math.max(xMax, x);
+    yMax = Math.max(yMax, y);
+  }
 
-// function gameOfLife(liveCells, steps) {
-//   printBoard(liveCells);
-//   for (let i = 0; i < steps; i++) {
-//     liveCells = iterate(liveCells);
-//     printBoard(liveCells);
-//   }
-// }
+  const board = [...Array(xMax + 1)].map(() => Array(yMax + 1).fill('.'));
+  for (let i = 0; i < liveCells.length; i++) {
+    const [x, y] = liveCells[i];
+    board[x][y] = '*';
+  }
+  return board;
+}
 
-// export default gameOfLife;
+/**
+ * Prints the board at this state
+ * @param {number[][]} liveCells
+ */
+function printBoard(liveCells) {
+  if (liveCells.length === 0) {
+    console.log('.');
+    return;
+  }
+
+  const board = createBoard(liveCells);
+
+  let result = '';
+  for (let r = 0; r < board.length; r++) {
+    const row = board[r];
+    result += `${row.join(' ')}\n`;
+  }
+  console.log(result);
+}
+
+/**
+ * Go through one cycle of life
+ * @param {number[][]} liveCells
+ * @return {number[][]}
+ */
+function iterate(liveCells) {
+  const board = createBoard(liveCells);
+  const newLiveCells = [];
+  const dir = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      let aliveNeighbors = 0;
+      for (let i = 0; i < dir.length; i++) {
+        const cellR = r + dir[i][0];
+        const cellC = c + dir[i][1];
+        if (
+          cellR >= 0 &&
+          cellC >= 0 &&
+          cellR < board.length &&
+          cellC < board[0].length &&
+          board[cellR][cellC] === '*'
+        ) {
+          aliveNeighbors++;
+        }
+      }
+
+      if (aliveNeighbors === 3) newLiveCells.push([r, c]);
+
+      const alive = board[r][c] === '*';
+      if (alive && aliveNeighbors === 2) newLiveCells.push([r, c]);
+    }
+  }
+
+  return newLiveCells;
+}
+
+/**
+ * Start with live cells. At each step, go through one iteration of life and prints the board
+ * @param {number[][]} liveCells
+ * @param {number} steps
+ */
+function gameOfLife(liveCells, steps) {
+  printBoard(liveCells);
+  for (let i = 0; i < steps; i++) {
+    liveCells = iterate(liveCells);
+    printBoard(liveCells);
+  }
+}
+
+export default gameOfLife;
