2840322e894bbea3b77f0cbbc4c78e5c10681790
Victor Li <victorli5674@yahoo.com>
2019-04-17 20:04:41 -0700

    93 Complete

--- solutions/91-100/Problem93.js
+++ solutions/91-100/Problem93.js
@@ -5,3 +5,74 @@
 // Given a tree, find the largest tree/subtree that is a BST.
 //
 // Given a tree, return the size of the largest tree/subtree that is a BST.
+
+/**
+ * Returns the largest tree/subtree that is a BST.
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+function largestBST(root) {
+  const largest = largestBSTHelper(root);
+  return largest.data;
+}
+
+/**
+ * Returns the size of the largest tree/subtree that is a BST.
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function sizeOfLargestBST(root) {
+  const largest = largestBSTHelper(root);
+  return largest.size;
+}
+
+class BSTInfo {
+  constructor(
+    min = Number.MAX_SAFE_INTEGER,
+    max = Number.MIN_SAFE_INTEGER,
+    isBST = true,
+    size = 0,
+    data = null
+  ) {
+    this.min = min;
+    this.max = max;
+    this.isBST = isBST;
+    this.size = size;
+    this.data = data;
+  }
+}
+
+/**
+ * Returns the information of the largest tree/subtree that is a BST
+ * @param {TreeNode} root
+ * @return {BSTInfo}
+ */
+function largestBSTHelper(root) {
+  if (root === null) return new BSTInfo();
+
+  const leftBSTInfo = largestBSTHelper(root.left);
+  const rightBSTInfo = largestBSTHelper(root.right);
+
+  if (
+    !leftBSTInfo.isBST ||
+    !rightBSTInfo.isBST ||
+    leftBSTInfo.max > root.val ||
+    rightBSTInfo.min <= root.val
+  ) {
+    const size = Math.max(leftBSTInfo.size, rightBSTInfo.size);
+    // store largest bst so far
+    const BST =
+      leftBSTInfo.size > rightBSTInfo.size
+        ? leftBSTInfo.data
+        : rightBSTInfo.data;
+    // min and max does not matter because of isBST false property
+    return new BSTInfo(-1, -1, false, size, BST);
+  }
+
+  const size = leftBSTInfo.size + rightBSTInfo.size + 1;
+  const min = root.left !== null ? leftBSTInfo.min : root.val;
+  const max = root.right !== null ? rightBSTInfo.max : root.val;
+  return new BSTInfo(min, max, true, size, root);
+}
+
+export { largestBST, sizeOfLargestBST };
--- solutions/__tests__/91.100.test.js
+++ solutions/__tests__/91.100.test.js
@@ -1,4 +1,7 @@
+import TreeNode from '../Data-Structures/TreeNode';
+
 import courseOrder from '../91-100/Problem92';
+import { largestBST, sizeOfLargestBST } from '../91-100/Problem93';
 
 describe('Problems 91 - 100', () => {
   test('Problem 92 Course Ordering', () => {
@@ -10,5 +13,37 @@ describe('Problems 91 - 100', () => {
 
     const map = new Map(kv);
     expect(courseOrder(map)).toEqual(['CSC100', 'CSC200', 'CSC300']);
+  });
+
+  test('Problem 93 Largest BST Subtree', () => {
+    const root = new TreeNode(10);
+    root.left = new TreeNode(5);
+    root.right = new TreeNode(15);
+    root.left.left = new TreeNode(1);
+    root.left.right = new TreeNode(8);
+    root.right.right = new TreeNode(7);
+
+    expect(largestBST(root)).toEqual(root.left);
+    expect(sizeOfLargestBST(root)).toBe(3);
+
+    const root1 = new TreeNode(5);
+    root1.left = new TreeNode(2);
+    root1.right = new TreeNode(4);
+    root1.left.left = new TreeNode(1);
+    root1.left.right = new TreeNode(3);
+    expect(largestBST(root1)).toEqual(root1.left);
+    expect(sizeOfLargestBST(root1)).toBe(3);
+
+    const root2 = new TreeNode(50);
+    root2.left = new TreeNode(30);
+    root2.right = new TreeNode(60);
+    root2.left.left = new TreeNode(5);
+    root2.left.right = new TreeNode(20);
+    root2.right.left = new TreeNode(45);
+    root2.right.right = new TreeNode(70);
+    root2.right.right.left = new TreeNode(65);
+    root2.right.right.right = new TreeNode(80);
+    expect(largestBST(root2)).toEqual(root2.right);
+    expect(sizeOfLargestBST(root2)).toBe(5);
   });
 });
